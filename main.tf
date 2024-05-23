82d9c022f08a1a438cc1d81865af5c71b534a8f9
Arash <arash.khorsand@gmail.com>
2024-05-23 16:48:05 -0400

    adding ec2 resource & tag part

--- main.tf
+++ main.tf
@@ -12,3 +12,13 @@ terraform {
 provider "aws" {
   region  = "ca-central-1"
 }
+
+resource "aws_instance" "terr-1" 
+{
+    ami     = "ami-0c4596ce1e7ae3e68" #last ubuntu ami_id in aws
+    instance_type= "t2.micro" #for t2.micro aws
+    
+    tags = {
+    created_by = "Arash"
+    }    
+}
