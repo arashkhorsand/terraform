69b55292656f60e1ef064428826bb4674bd78c3c
Arash <arash.khorsand@gmail.com>
2024-05-24 13:44:13 -0400

    adding variables

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "terr-1" {
     key_name = var.key_name #variable key_name in variables.tf
     instance_type= var.instance_type #variable instance_type in variables.tf
     tags = {
-    Name= "arash-terr"
+    Name= var.machine_name
     created_by = "Arash"
     }   
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
 variable "ami_id" {default="ami-0c4596ce1e7ae3e68"}
 variable "instance_type" {default="t2.micro"}
 variable "key_name" {default="docker-server"}
+variable "machine_name" {}
