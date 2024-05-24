b0bfc290c1520803e8a343bed6f6ff3fb4d1eb45
Arash <arash.khorsand@gmail.com>
2024-05-24 15:21:53 -0400

    add type and description to variables

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
-variable "ami_id" {type = string
-description= "this variable for ami id"}
+variable "ami_id" {
+    type = string 
+    description= "this variable for ami id"}
 variable "instance_type" {}
 variable "key_name" {}
 variable "machine_name" {}
