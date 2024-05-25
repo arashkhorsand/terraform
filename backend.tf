66d18838328e0be76995251aff76852f6987a900
Arash <arash.khorsand@gmail.com>
2024-05-25 13:30:37 -0400

    reverse seperate file change

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-backend "s3" {
-    bucket = "terraform-arash"
-    key    = "path/terraform.tfstate"
-    region = "ca-central-1"
-}
-}
--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 terraform {
-      backend "s3" {
+  backend "s3" {
     bucket = "terraform-arash"
     key    = "path/terraform.tfstate"
     region = "ca-central-1"
-  }
+}
   required_providers {
     aws = {
       source  = "hashicorp/aws"
